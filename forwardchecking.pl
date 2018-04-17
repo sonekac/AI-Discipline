cacbd3e86fa629b8586db4de93eaf04b0633fe13
sonekac <soneka@DESKTOP-AHUBNNI.localdomain>
2018-04-17 19:00:50 +0100

    Forward Checking está funcionar

--- forwardchecking.pl
+++ forwardchecking.pl
@@ -1,4 +1,3 @@
-
 quadrantes([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]]).
 quadrantes([[1,4],[1,5],[1,6],[2,4],[2,5],[2,6],[3,4],[3,5],[3,6]]).
 quadrantes([[1,7],[1,8],[1,9],[2,7],[2,8],[2,9],[3,7],[3,8],[3,9]]).
@@ -12,8 +11,9 @@ quadrantes([[7,7],[7,8],[7,9],[8,7],[8,8],[8,9],[9,7],[9,8],[9,9]]).
 p:- estado_inicial(E0), back(E0,A), sort(A), escreve(A).
 
 back(e([],A),A).
-back(E,Sol) :- sucessor(E,e(A,F)), ve_restricoes(e(A,F)),% write(F), nl,
-                          back(e(A,F),Sol).
+back(E,Sol) :-  sucessor(E,E1), ve_restricoes(E1),
+                propagacao_restricoes(E1, E3), back(E3,Sol).
+
 
 sucessor(e([v(N,D,V)|R],E),e(R,[v(N,D,V)|E])):- member(V,D).
 
@@ -60,7 +60,7 @@ ve_restricoesl(v([X,Y],_,Vi),Afect) :- member(v([X,Z],_,Vi), Afect), !,fail.
 ve_restricoesl(_, _).
 ve_restricoesc(v([X,Y],_,Vi),Afect) :- member(v([Z,Y],_,Vi), Afect), !,fail.
 ve_restricoesc(_,_).
-quadrante(v([X,Y],Di,Vi),Afect) :- quadrantes(A), member([X,Y],A),member([X1,Y1],A), member(v([X1,Y1],_,Vi),Afect), !,fail.
+quadrante(v([X,Y],_,Vi),Afect) :- quadrantes(A), member([X,Y],A),member([X1,Y1],A), member(v([X1,Y1],_,Vi),Afect), !,fail.
 quadrante(_,_).
 
 escreve([]).
@@ -68,3 +68,32 @@ escreve([v([X,9],_,Vi)|Afect]) :- T is X mod 3, T = 0, write(Vi), nl,write('----
 escreve([v([_,9],_,Vi)|Afect]) :- write(Vi), nl ,escreve(Afect).
 escreve([v([_,Y],_,Vi)|Afect]) :- T is Y mod 3, T = 0, write(Vi),write(' | ') ,escreve(Afect).
 escreve([v(_,_,Vi)|Afect]) :-  write(Vi), write(' '), escreve(Afect).
+
+
+
+
+
+propagacao_restricoes(e(R,[v(I,Di,Vi)|L]), e(R3, [v(I,Di,Vi)|L])) :-
+                                    quadrante_arc(v(I,Di,Vi), R, [], T),
+                                    linha_arc(v(I,Di,Vi), T, [], T1),
+                                    quadrante_arc(v(I,Di,Vi), T1, [], R3).
+
+linha_arc(_,[], A, A).
+linha_arc(v([X,Y],_,Vi), [v([X,Y1],Di,Vj)|R], R1, T) :- !,rem(Vi, Di, Dj),
+                                                        linha_arc(v([X,Y],_,Vi), R, [v([X,Y1],Dj,Vj)|R1], T).
+linha_arc(v([X,Y],_,Vi), [v([X1,Y1],Di,Vj)|R], R1, T) :-  linha_arc(v([X,Y],_,Vi), R, [v([X1,Y1],Di,Vj)|R1], T).
+
+coluna_arc(_,[], A, A).
+coluna_arc(v([X,Y],_,Vi), [v([X1,Y],Di,Vj)|R], R1, T) :- !, rem(Vi, Di, Dj),
+                                                          coluna_arc(v([X,Y],_,Vi), R, [v([X1,Y],Dj,Vj)|R1], T).
+coluna_arc(v([X,Y],_,Vi), [v([X1,Y1],Di,Vj)|R], R1, T) :- coluna_arc(v([X,Y],_,Vi), R, [v([X1,Y1],Di,Vj)|R1], T).
+
+quadrante_arc(_,[], A, A).
+quadrante_arc(v([X,Y],_,Vi), [v([X1,Y1],Di,Vj)|R], R1, T) :- quadrantes(A), member([X,Y],A), member([X1,Y1], A), !,
+                                                             rem(Vi, Di, Dj), quadrante_arc(v([X,Y],_,Vi), R, [v([X1,Y1],Dj,Vj)|R1], T).
+quadrante_arc(v([X,Y],_,Vi), [v([X1,Y1],Di,Vj)|R], R1, T) :-quadrante_arc(v([X,Y],_,Vi), R, [v([X1,Y1],Di,Vj)|R1], T).
+
+
+rem(_, [], []).
+rem(Item, [Item|L],L) :- !.
+rem(Item, [X|XT],[X|YT]) :- rem(Item, XT, YT).
